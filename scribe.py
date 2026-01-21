c212a94f03fd0e0cd846722836d15736f1c03e11
satyendra kumar <101421206+sat78@users.noreply.github.com>
2026-01-21 16:16:42 +0530

    Update scribe.py

--- scribe.py
+++ scribe.py
@@ -5,7 +5,7 @@ import os
 import tempfile
 from datetime import datetime
 #from moviepy.editor import VideoFileClip
-import moviepy.editor as mp
+#import moviepy.editor as mp
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from pydub import AudioSegment
 from pydub.utils import make_chunks
@@ -1033,3 +1033,4 @@ st.markdown("""
     </div>
 
 """, unsafe_allow_html=True)
+
